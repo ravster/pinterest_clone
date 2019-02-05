b94079f9195d2e5853d42b28dbd4c238d402323d
Ravi Desai <ravi@ravidesai.com>
2019-02-04 19:08:59 -0500

    Get userid from authorization token
    Also, get token from the header

--- main.go
+++ main.go
@@ -36,6 +36,31 @@ var db *sql.DB
 // Image - gen-short-url
 // Image - List by user
 
+func getUserIdFromToken(db *sql.DB, token string) (string, err) {
+	fmt.Printf("Attempting to get user-id for token %s\n", token)
+	query := fmt.Sprintf(`SELECT id
+FROM users
+WHERE token = '%s'
+LIMIT 1`,
+		token)
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var userid string
+		if err := rows.Scan(&userid); err != nil {
+			log.Print(err)
+			return nil, err
+		}
+
+		return userid, nil
+	}
+}
+
 func saveNewImage(db *sql.DB, userID, href string) (err error) {
 	fmt.Printf("Inserting %s for %s\n", href, userID)
 	sql_query := fmt.Sprintf(`INSERT INTO images (href, user_id)
@@ -51,7 +76,22 @@ VALUES ('href', 'userID')`)
 }
 
 func createNewImage(c *gin.Context) {
+	// Get token from context
+	token := c.GetHeader("Authorization")
+	if token == "" {
+		c.JSON(401, gin.H{
+			"error": "Missing Authorization token",
+		})
+		return
+	}
 	// Get user-id from token
+	userId, err := getUserIdFromToken(token)
+	if err != nil {
+		c.JSON(401, gin.H{
+			"error": "Invalid token",
+		})
+		return
+	}
 	// Get href string from req-body
 
 	// Save into DB
@@ -60,6 +100,7 @@ func createNewImage(c *gin.Context) {
 		c.JSON(422, gin.H{
 			"error": "Couldn't save image to DB",
 		})
+		return
 	}
 
 	c.JSON(201, gin.H{})
