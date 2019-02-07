fe8bd688fcf5bf3850799aca740ed3966386e241
Ravi Desai <ravi@ravidesai.com>
2019-02-06 19:25:34 -0500

    Move all interaction with the database to another package
    This change allows a seperation of concerns, so the "db" package is
    the only one that needs to deal with knowledge of the database.  This
    way the rest of the code of the program can limit itself to the domain
    concerns.

---
+++ db/db.go
@@ -0,0 +1,91 @@
+package db
+
+import (
+	"os"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"fmt"
+	"errors"
+)
+
+// DB_URL="postgresql://postgres:password@db:5432/pc"
+var db_url = os.Getenv("DB_URL")
+var DB *sql.DB
+
+func Connect() {
+	if DB != nil {
+		DB.Close()
+	}
+
+	var err error
+	log.Println("connecting to postgres database... ")
+	DB, err = sql.Open("postgres", db_url)
+
+	if err != nil {
+		log.Panicf(`failed to connect database. err="%v"`, err)
+	}
+}
+
+func GetUserIdFromToken(token string) (string, error) {
+	fmt.Printf("Attempting to get user-id for token %s\n", token)
+	query := fmt.Sprintf(`SELECT id
+FROM users
+WHERE token = '%s'
+LIMIT 1`,
+		token)
+	// TODO: Check the token has not expired.
+
+	rows, err := DB.Query(query)
+	if err != nil {
+		log.Print(err)
+		return "", err
+	}
+	defer rows.Close()
+
+	var userid string
+	for rows.Next() {
+		if err := rows.Scan(&userid); err != nil {
+			log.Print(err)
+			return "", err
+		}
+	}
+	if userid == "" {
+		return "", errors.New("Token not found")
+	}
+
+	return userid, nil
+}
+
+func SaveNewImage(userID, href string) (err error) {
+	fmt.Printf("Inserting %s for %s\n", href, userID)
+	sql_query := fmt.Sprintf(`INSERT INTO images (href, user_id, created_at, updated_at)
+VALUES ('%s', '%s', now(), now())`,
+		href,
+		userID)
+
+	rows, err := DB.Query(sql_query)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	rows.Close()
+	return nil
+}
+
+func MarkImageDeleted(userId, imageId string) error {
+	fmt.Printf("Deleting image %s\n", imageId)
+	sql_query := fmt.Sprintf(`UPDATE images set deleted_at = now()
+WHERE user_id = '%s' AND id = '%s'`,
+		userId,
+		imageId,
+	)
+
+	rows, err := DB.Query(sql_query)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	rows.Close()
+	return nil
+}
--- main.go
+++ main.go
@@ -1,89 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	// "strconv"
-	"errors"
-	"database/sql"
-
-	_ "github.com/lib/pq"
 	"github.com/gin-gonic/gin"
-)
 
-// DB_URL="postgresql://postgres:password@db:5432/pc"
-var db_url = os.Getenv("DB_URL")
-var db *sql.DB
+	db "github.com/ravster/pinterest_clone/db"
+)
 
 // Add Seed data?
 // Return authenticated user
 // Image - gen-short-url
 // Image - List by user
-
-func getUserIdFromToken(db *sql.DB, token string) (string, error) {
-	fmt.Printf("Attempting to get user-id for token %s\n", token)
-	query := fmt.Sprintf(`SELECT id
-FROM users
-WHERE token = '%s'
-LIMIT 1`,
-		token)
-	// TODO: Check the token has not expired.
-
-	rows, err := db.Query(query)
-	if err != nil {
-		log.Print(err)
-		return "", err
-	}
-	defer rows.Close()
-
-	var userid string
-	for rows.Next() {
-		if err := rows.Scan(&userid); err != nil {
-			log.Print(err)
-			return "", err
-		}
-	}
-	if userid == "" {
-		return "", errors.New("Token not found")
-	}
-
-	return userid, nil
-}
-
-func saveNewImage(db *sql.DB, userID, href string) (err error) {
-	fmt.Printf("Inserting %s for %s\n", href, userID)
-	sql_query := fmt.Sprintf(`INSERT INTO images (href, user_id, created_at, updated_at)
-VALUES ('%s', '%s', now(), now())`,
-		href,
-		userID)
-
-	rows, err := db.Query(sql_query)
-	if err != nil {
-		log.Print(err)
-		return err
-	}
-	rows.Close()
-	return nil
-}
-
-func markImageDeleted(db *sql.DB, userId, imageId string) error {
-	fmt.Printf("Deleting image %s\n", imageId)
-	sql_query := fmt.Sprintf(`UPDATE images set deleted_at = now()
-WHERE user_id = '%s' AND id = '%s'`,
-		userId,
-		imageId,
-	)
-
-	rows, err := db.Query(sql_query)
-	if err != nil {
-		log.Print(err)
-		return err
-	}
-	rows.Close()
-	return nil
-}
-
 
 func createNewImage(c *gin.Context) {
 	// Get token from context
@@ -95,7 +21,7 @@ func createNewImage(c *gin.Context) {
 		return
 	}
 	// Get user-id from token
-	userId, err := getUserIdFromToken(db, token)
+	userId, err := db.GetUserIdFromToken(token)
 	if err != nil {
 		c.JSON(401, gin.H{
 			"error": "Invalid token",
@@ -119,8 +45,7 @@ func createNewImage(c *gin.Context) {
 		return
 	}
 
-	// Save into DB
-	err = saveNewImage(db, userId, href)
+	err = db.SaveNewImage(userId, href)
 	if err != nil {
 		c.JSON(422, gin.H{
 			"error": "Couldn't save image to DB",
@@ -141,7 +66,7 @@ func deleteImage(c *gin.Context) {
 		return
 	}
 	// Get user-id from token
-	userId, err := getUserIdFromToken(db, token)
+	userId, err := db.GetUserIdFromToken(token)
 	if err != nil {
 		c.JSON(401, gin.H{
 			"error": "Invalid token",
@@ -153,7 +78,7 @@ func deleteImage(c *gin.Context) {
 	imageId := c.Param("id")
 
 	// mark image deleted in DB
-	err = markImageDeleted(db, userId, imageId)
+	err = db.MarkImageDeleted(userId, imageId)
 	if err != nil {
 		c.JSON(422, gin.H{
 			"error": "Couldn't delete image",
@@ -172,11 +97,7 @@ func pong(c *gin.Context) {
 }
 
 func main() {
-	var err error
-	db, err = sql.Open("postgres", db_url)
-	if err != nil {
-		log.Fatal(err)
-	}
+	db.Connect()
 
 	r := gin.Default()
 	r.GET("/ping", pong)
